dc4e400c8b6c501e72b07f097b81424b121f4166
jumair <jumairahmad@gmail.com>
2024-09-02 15:01:41 +0500

    Initial Commit

---
+++ iosdevcourse/iosdevcourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosdevcourseApp.swift
+//  iosdevcourse
+//
+//  Created by MacBook Pro on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct iosdevcourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
