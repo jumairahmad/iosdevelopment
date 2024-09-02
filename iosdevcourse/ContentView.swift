3cf4a93246ac7d829f1186bc462bb5833064e06f
jumair <jumairahmad@gmail.com>
2024-09-02 15:17:46 +0500

    day 1

--- iosdevcourse/ContentView.swift
+++ iosdevcourse/ContentView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment:.leading) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            HStack{
+                Text("jemica").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("islamabad").font(.subheadline)
+                    .foregroundStyle(Color(UIColor.red))
+                Spacer()
+                Text("Delhi")
+            }
         }
         .padding()
     }
